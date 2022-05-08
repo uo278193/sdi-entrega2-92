fb80dbb20329bc117741c8432d5c14a50042d1b2
Rosa <uo277921@uniovi.es>
2022-05-08 21:58:34 +0200

    REST api completada ?

---
+++ src/main/repositories/messagesRepository.js
@@ -0,0 +1,45 @@
+module.exports = {
+    mongoClient: null,
+    app: null,
+    init: function (app, mongoClient) {
+        this.mongoClient = mongoClient;
+        this.app = app;
+    },
+    getMessages: async function (filter, options) {
+        try {
+            const client = await this.mongoClient.connect(this.app.get('connectionStrings'));
+            const database = client.db("sdistagram");
+            const collectionName = 'messages';
+            const messagesCollection = database.collection(collectionName);
+            const messages = await messagesCollection.find(filter, options).toArray();
+            return messages;
+        } catch (error) {
+            throw (error);
+        }
+    }, insertMessage: function (comment, callbackFunction) {
+        this.mongoClient.connect(this.app.get('connectionStrings'), function (err, dbClient) {
+            if (err) {
+                callbackFunction(null)
+            } else {
+                const database = dbClient.db("sdistagram");
+                const collectionName = 'messages';
+                const messagesCollection = database.collection(collectionName);
+                messagesCollection.insertOne(comment)
+                    .then(result => callbackFunction(result.insertedId))
+                    .then(() => dbClient.close())
+                    .catch(err => callbackFunction({error: err.message}));
+            }
+        });
+    }, updateMessage: async function (newMessage, filter, options) {
+        try {
+            const client = await this.mongoClient.connect(this.app.get('connectionStrings'));
+            const database = client.db("sdistagram");
+            const collectionName = 'messages';
+            const messagesCollection = database.collection(collectionName);
+            const result = await messagesCollection.updateOne(filter, {$set: newMessage}, options);
+            return result;
+        } catch (error) {
+            throw (error);
+        }
+    }
+};
